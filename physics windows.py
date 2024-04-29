858ac89164457a95a8dc070978af55639b535006
rufus173 <derrickotron5000@gmail.com>
2024-04-29 23:05:15 +0100

    added a 3rd window

--- physics windows.py
+++ physics windows.py
@@ -18,13 +18,18 @@ class main(window_handler):
         super().__init__()
         self.new_window("window 1")
         self.new_window("window 2")
-        self.windows["window 1"]["button"] = tkinter.Button(text="button",command=self.trigger)
+        self.new_window("window 3")
+        self.windows["window 1"]["button"] = tkinter.Button(self.windows["window 1"]["root"],text="button",command=self.trigger1)
         self.windows["window 1"]["button"].pack()
+        self.windows["window 3"]["button"] = tkinter.Button(self.windows["window 3"]["root"],text="button",command=self.trigger2)
+        self.windows["window 3"]["button"].pack()
         self.windows["window 2"]["text"] = tkinter.Text(self.windows["window 2"]["root"])
         self.windows["window 2"]["text"].pack()
         self.mainloop()
-    def trigger(self):
-        self.windows["window 2"]["text"].insert(tkinter.END,"\nbased")
+    def trigger1(self):
+        self.windows["window 2"]["text"].insert(tkinter.END,"based\n")
+    def trigger2(self):
+        self.windows["window 2"]["text"].insert(tkinter.END,"cringe\n")
     def mainloop(self):
         while True:
             self.update_windows()
